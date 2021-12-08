f8cdfef7cd4f4031af949da58220eebdd68d464d
yyhyap <yongheng98@hotmail.com>
2021-12-08 21:33:57 +0800

    Test Allocation Failure and Partial Allocation

--- src/test/java/com/yyh/msscbeerorderservice/services/testcomponents/BeerOrderAllocationListener.java
+++ src/test/java/com/yyh/msscbeerorderservice/services/testcomponents/BeerOrderAllocationListener.java
@@ -23,16 +23,39 @@ public class BeerOrderAllocationListener {
     public void listen(Message message) {
         AllocateOrderRequest request = (AllocateOrderRequest) message.getPayload();
 
+        boolean pendingInventory = false;
+        boolean allocationError = false;
+        boolean sendResponse = true;
+
         System.out.println("##### Allocation Listener RAN #####");
 
+        //set allocation error
+        if (request.getBeerOrderDto().getCustomerRef() != null) {
+            if (request.getBeerOrderDto().getCustomerRef().equals("fail-allocation")){
+                allocationError = true;
+            }  else if (request.getBeerOrderDto().getCustomerRef().equals("partial-allocation")) {
+                pendingInventory = true;
+            } else if (request.getBeerOrderDto().getCustomerRef().equals("dont-allocate")){
+                sendResponse = false;
+            }
+        }
+
+        boolean finalPendingInventory = pendingInventory;
+
         request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
-            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            if (finalPendingInventory) {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity() - 1);
+            } else {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            }
         });
 
-        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, AllocateOrderResult.builder()
-                        .beerOrderDto(request.getBeerOrderDto())
-                        .allocationError(false)
-                        .pendingInventory(false)
-                .build());
+        if(sendResponse) {
+            jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, AllocateOrderResult.builder()
+                    .beerOrderDto(request.getBeerOrderDto())
+                    .allocationError(allocationError)
+                    .pendingInventory(pendingInventory)
+                    .build());
+        }
     }
 }
--- src/test/java/com/yyh/msscbeerorderservice/services/testcomponents/BeerOrderValidationListener.java
+++ src/test/java/com/yyh/msscbeerorderservice/services/testcomponents/BeerOrderValidationListener.java
@@ -22,20 +22,32 @@ public class BeerOrderValidationListener {
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
     public void listen(Message message) {
         boolean isValid = true;
+        boolean sendResponse = true;
 
         ValidateOrderRequest request = (ValidateOrderRequest) message.getPayload();
 
         System.out.println("##### Validation Listener RAN #####");
 
-        // condition to fail validation
-        if(request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("fail-validation")) {
-            isValid = false;
+//        // condition to fail validation
+//        if(request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("fail-validation")) {
+//            isValid = false;
+//        }
+
+        if(request.getBeerOrderDto().getCustomerRef() != null) {
+            // condition to fail validation
+            if(request.getBeerOrderDto().getCustomerRef().equals("fail-validation")) {
+                isValid = false;
+            } else if (request.getBeerOrderDto().getCustomerRef().equals("dont-validate")){
+                sendResponse = false;
+            }
         }
 
-        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE, ValidateOrderResult.builder()
-                        .isValid(isValid)
-                        .orderId(request.getBeerOrderDto().getId())
-                .build());
+        if(sendResponse) {
+            jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE, ValidateOrderResult.builder()
+                    .isValid(isValid)
+                    .orderId(request.getBeerOrderDto().getId())
+                    .build());
+        }
 
     }
 
